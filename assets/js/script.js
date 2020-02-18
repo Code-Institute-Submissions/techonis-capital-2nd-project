7f343c2c0fcb127741663849f63c7e73823b657d
rcesonis <rimvydas.cesonis@gmail.com>
2020-02-18 20:34:48 +0100

    working with graphs

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,76 @@ $('.filter-button-group').on( 'click', 'button', function(){
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
+
+/*** making small graphs ***/
+
+// function to make graphs
+var data1 = [[15, 19, 22, 5, 22], [12, 15, 65, 25, 62], [18, 1, 25, 24, 1], [12, 25, 42, 42, 5], 
+[12, 15, 65, 25, 62], [12, 15, 65, 25, 62], [12, 15, 65, 25, 62], [12, 15, 65, 25, 62], [12, 15, 65, 25, 62]];
+var chartNr =['myChart-1', 'myChart-2', 'myChart-3'];
+
+function makeCharts(data, chartNr) {
+  var ctx = document.getElementById(chartNr).getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: [],
+          datasets: [{
+              label: 'object price',
+              data: data,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)'
+              ],
+              borderColor: [
+                  '#72223b'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+}
+
+
+$(".stat-1").click(function(){
+    makeCharts(data1[0], chartNr[0]);
+  });
+$(".stat-2").click(function(){
+makeCharts(data1[1], chartNr[1]);
+  });
+$(".stat-3").click(function(){
+makeCharts(data1[2], chartNr[2]);
+  });
+$(".stat-4").click(function(){
+makeCharts(data1[3], chartNr[3]);
+  });
+$(".stat-5").click(function(){
+makeCharts(data1[4], chartNr[4]);
+});
+$(".stat-6").click(function(){
+makeCharts(data1[5], chartNr[5]);
+  });
+$(".stat-7").click(function(){
+makeCharts(data1[6], chartNr[6]);
+  });
+$(".stat-8").click(function(){
+makeCharts(data1[7], chartNr[7]);
+  });
+$(".stat-8").click(function(){
+makeCharts(data1[8], chartNr[8]);
+  });
+$(".stat-9").click(function(){
+makeCharts(data1[9], chartNr[9]);
+  });
+// loop through HTML class
+// loop through data array
+// assign data to graph
+
