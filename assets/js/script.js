e8d5426fa867bee75e0930ea084937cf5db6d260
rcesonis <rimvydas.cesonis@gmail.com>
2020-04-14 22:21:12 +0200

    rewriting chart in more effective way

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,10 @@ $('#object-list a').on('click', function (e) {
     $(this).tab('show')
   });
 
-/* init isotope */
+/******************************************
+ * Isotope.js
+ *****************************************/
+ 
 var $grid = $('.grid').isotope({
   // options
 });
@@ -14,75 +17,86 @@ $('.filter-button-group').on( 'click', 'button', function(){
   $grid.isotope({ filter: filterValue });
 });
 
-/*** making small graphs ***/
+/*********************************************/
+
+/*********************************************
+ * Charts.js
+ ********************************************/
+// small charts for cards 
+
+var chartColors = 'rgba(114,34,59, 0.4)';
+var chartBorder = 'rgba(114,34,59)';
+
+chartconfig = {
+  type: 'line',
+  data: {
+    datasets: [{
+      label: 'Property price by year: '
+    }]
+  }
+};
+
+window.onload = function() {
+	var ctx = document.getElementById('smallChart-1').getContext('2d');
+	window.myChart = new Chart(ctx, chartconfig);
+};
 
 // function to make graphs
-var data1 = [[15, 19, 22, 5, 22], [12, 15, 65, 25, 62], [18, 1, 25, 24, 1], [12, 25, Math.floor(Math.random() * 100), 42, 5], [12, Math.floor(Math.random() * 100), 65, 25, 62], [12, 15, 65, Math.floor(Math.random() * 100), 62], [12, 15, Math.floor(Math.random() * 100), 25, 62], [12, Math.floor(Math.random() * 100), 65, 25, 62], [Math.floor(Math.random() * 100), 15, Math.floor(Math.random() * 100), 25, 62]];
-var chartNr =['myChart-1', 'myChart-2', 'myChart-3', 
-'myChart-4', 'myChart-5', 'myChart-6', 'myChart-7', 'myChart-8', 'myChart-9'];
+// var data1 = [[15, 19, 22, 5, 22], [12, 15, 65, 25, 62], [18, 1, 25, 24, 1], [12, 25, Math.floor(Math.random() * 100), 42, 5], [12, Math.floor(Math.random() * 100), 65, 25, 62], [12, 15, 65, Math.floor(Math.random() * 100), 62], [12, 15, Math.floor(Math.random() * 100), 25, 62], [12, Math.floor(Math.random() * 100), 65, 25, 62], [Math.floor(Math.random() * 100), 15, Math.floor(Math.random() * 100), 25, 62]];
+// var chartNr =['myChart-1', 'myChart-2', 'myChart-3', 
+// 'myChart-4', 'myChart-5', 'myChart-6', 'myChart-7', 'myChart-8', 'myChart-9'];
 
-function makeCharts(data, chartNr) {
-  var ctx = document.getElementById(chartNr).getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          labels: ['1995', '2000', '2005', '2010', '2020'],
-          datasets: [{
-              label: 'object price',
-              cubicInterpolationMode: 'monotone',
-              data: data,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.4)'
-              ],
-              borderColor: [
-                  '#72223b'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
-}
+// function makeCharts(data, chartNr) {
+//   var ctx = document.getElementById(chartNr).getContext('2d');
+//   var myChart = new Chart(ctx, {
+//       type: 'line',
+//       data: {
+//           labels: ['1995', '2000', '2005', '2010', '2020'],
+//           datasets: [{
+//               label: 'object price',
+//               cubicInterpolationMode: 'monotone',
+//               data: data,
+//               backgroundColor: [
+//                   'rgba(255, 99, 132, 0.4)'
+//               ],
+//               borderColor: [
+//                   '#72223b'
+//               ],
+//               borderWidth: 1
+//           }]
+//       },
+//       options: {
+//           scales: {
+//               yAxes: [{
+//                   ticks: {
+//                       beginAtZero: true
+//                   }
+//               }]
+//           }
+//       }
+//   });
+// }
 /*** Main big chart ***/
 
-function makeBigChart() {
-  var ctx = document.getElementById('bigChart').getContext('2d');
-  var bigChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          labels: ['1995', '2000', '2005', '2010', '2020'],
-          datasets: [{
-              label: 'object price',
-              cubicInterpolationMode: 'monotone',
-              data: [12, 15, 62, 63 , 25],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)'
-              ],
-              borderColor: [
-                  '#72223b'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
-}
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+
+  type: 'bar',
+
+  plugins: [ChartDataSource],
+
+  options: {
+
+    datasource: {
+
+      url: 'dataset.xlsx'
+
+    }
+
+  }
+
+});
 
 $(document).ready(function() {
   makeBigChart();
