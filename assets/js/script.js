2a2052d47c083056d30c5f0e18265650cdf71680
rcesonis <rimvydas.cesonis@gmail.com>
2020-04-15 02:18:20 +0200

    refactored small charts

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,122 +26,106 @@ $('.filter-button-group').on( 'click', 'button', function(){
 
 var chartColor = 'rgba(114,34,59, 0.4)';
 var chartBorder = 'rgba(114,34,59)';
+var chartNr = [];
+var count = 1;
+var data;
+var myChart;
+var object_title;
 
-chartconfig = {
-  type: 'line',
-  data: {
-    datasets: [{
-      label: 'Property price by year: ',
-      backgroundColor: chartColor,
-      borderColor: chartBorder,
-      borderWidth: 1
-    }]
-  },
-  plugins: [ChartDataSource],
-  options: {
-    plugins: {
-      datasource: {
-        type: 'sheet',
-        url: 'dataset.xlsx',
-        datasetLabels: 'Sheet1!A2',
-        indexLabels: 'Sheet1!B1:G1',
-        data: 'Sheet1!B2:G2'
-      }
-    }
+
+function createChartNr(){
+  while (count < 10){
+    let result = 'smallChart-' + count;
+    chartNr.push(result);
+    count++;
   }
 };
+createChartNr();
 
-window.onload = function() {
-	var ctx = document.getElementById('smallChart-1').getContext('2d');
-	window.myChart = new Chart(ctx, chartconfig);
+
+function drawSmallCharts(chartNr){
+  var ctx = document.getElementById(chartNr).getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          datasets: [{
+              label: 'Property price',
+              cubicInterpolationMode: 'monotone',
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.4)'
+              ],
+              borderColor: [
+                  '#72223b'
+              ],
+              borderWidth: 1
+          }]
+      },
+      plugins: [ChartDataSource],
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          },
+        plugins: {
+          datasource: {
+            type: 'sheet',
+            url: 'dataset.xlsx',
+            datasetLabels: object_title,
+            indexLabels: 'Sheet1!B1:G1',
+            data: data,
+          }
+
+        }
+      }
+  })
 };
 
-// function to make graphs
-// var data1 = [[15, 19, 22, 5, 22], [12, 15, 65, 25, 62], [18, 1, 25, 24, 1], [12, 25, Math.floor(Math.random() * 100), 42, 5], [12, Math.floor(Math.random() * 100), 65, 25, 62], [12, 15, 65, Math.floor(Math.random() * 100), 62], [12, 15, Math.floor(Math.random() * 100), 25, 62], [12, Math.floor(Math.random() * 100), 65, 25, 62], [Math.floor(Math.random() * 100), 15, Math.floor(Math.random() * 100), 25, 62]];
-// var chartNr =['myChart-1', 'myChart-2', 'myChart-3', 
-// 'myChart-4', 'myChart-5', 'myChart-6', 'myChart-7', 'myChart-8', 'myChart-9'];
 
-// function makeCharts(data, chartNr) {
-//   var ctx = document.getElementById(chartNr).getContext('2d');
-//   var myChart = new Chart(ctx, {
-//       type: 'line',
-//       data: {
-//           labels: ['1995', '2000', '2005', '2010', '2020'],
-//           datasets: [{
-//               label: 'object price',
-//               cubicInterpolationMode: 'monotone',
-//               data: data,
-//               backgroundColor: [
-//                   'rgba(255, 99, 132, 0.4)'
-//               ],
-//               borderColor: [
-//                   '#72223b'
-//               ],
-//               borderWidth: 1
-//           }]
-//       },
-//       options: {
-//           scales: {
-//               yAxes: [{
-//                   ticks: {
-//                       beginAtZero: true
-//                   }
-//               }]
-//           }
-//       }
-//   });
-// }
-/*** Main big chart ***/
-
-// var ctx = document.getElementById('myChart').getContext('2d');
-
-// var chart = new Chart(ctx, {
-
-//   type: 'bar',
-
-//   plugins: [ChartDataSource],
-
-//   options: {
-
-//     datasource: {
-
-//       url: 'dataset.xlsx'
-
-//     }
-
-//   }
-
-// });
-
-// $(document).ready(function() {
-//   makeBigChart();
-// });
-// $(".stat-1").click(function(){
-//     makeCharts(data1[0], chartNr[0]);
-//   });
-// $(".stat-2").click(function(){
-// makeCharts(data1[1], chartNr[1]);
-//   });
-// $(".stat-3").click(function(){
-// makeCharts(data1[2], chartNr[2]);
-//   });
-// $(".stat-4").click(function(){
-// makeCharts(data1[3], chartNr[3]);
-//   });
-// $(".stat-5").click(function(){
-// makeCharts(data1[4], chartNr[4]);
-// });
-// $(".stat-6").click(function(){
-// makeCharts(data1[5], chartNr[5]);
-//   });
-// $(".stat-7").click(function(){
-// makeCharts(data1[6], chartNr[6]);
-//   });
-// $(".stat-8").click(function(){
-// makeCharts(data1[7], chartNr[7]);
-//   });
-// $(".stat-9").click(function(){
-// makeCharts(data1[8], chartNr[8]);
-//   });
-
-
+$(".stat-1").click(function(){
+  data = 'Sheet1!B1:G1';
+  object_title = 'Sheet1!A2';
+  drawSmallCharts(chartNr[0]);    
+});
+$(".stat-2").click(function(){
+  data = 'Sheet1!B2:G2';
+  object_title = 'Sheet1!A3';
+  drawSmallCharts(chartNr[1]);    
+});
+$(".stat-3").click(function(){
+  data = 'Sheet1!B3:G3';
+  object_title = 'Sheet1!A3';
+  drawSmallCharts(chartNr[2]);    
+});
+$(".stat-4").click(function(){
+  data = 'Sheet1!B4:G4';
+  object_title = 'Sheet1!A4';
+  drawSmallCharts(chartNr[3]);
+});
+$(".stat-5").click(function(){
+  data = 'Sheet1!B5:G5';
+  object_title = 'Sheet1!A5';
+  drawSmallCharts(chartNr[4]);    
+});
+$(".stat-6").click(function(){
+  data = 'Sheet1!B6:G6';
+  object_title = 'Sheet1!A6';
+  drawSmallCharts(chartNr[5]);    
+});
+$(".stat-7").click(function(){
+  data = 'Sheet1!B7:G7';
+  object_title = 'Sheet1!A7';
+  drawSmallCharts(chartNr[6]);    
+});
+$(".stat-8").click(function(){
+  data = 'Sheet1!B8:G8';
+  object_title = 'Sheet1!A8';
+  drawSmallCharts(chartNr[7]);
+});
+$(".stat-9").click(function(){
+  data = 'Sheet1!B9:G9';
+  object_title = 'Sheet1!A9';
+  drawSmallCharts(chartNr[8]);
+});
